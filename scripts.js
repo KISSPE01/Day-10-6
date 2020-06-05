e15151651ff9d4c2b9ddc8590912efc97d3c1ca8
KISSPE01 <Peter.K@upcmail.hu>
2020-06-05 19:16:57 +0200

    Publish

---
+++ scripts.js
@@ -0,0 +1,18 @@
+let names = ["Barna", "Gabor", "Anita", "Peti"];
+
+names.forEach(name => {
+    if (name === "Peti") { 
+        $('ul').append('<li><strong>'+name+'</strong></li>');
+    } else {
+        $('ul').append('<li>'+name+'</li>');
+    }
+})
+
+let additionalBlock = {
+    title: "Added with javascript",
+    text: "This block was added using JavaScript's jQuery library. How awesome!"
+};
+
+$('body').append('<h2>'+additionalBlock.title+'</h2>');
+$('body').append('<p>'+additionalBlock.text+'</p>');
+
